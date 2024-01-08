495283e1f81e88922d653cc14478b603f3f82de5
Renier <reniermin88@protonmail.com>
2024-01-08 13:12:38 -0700

    removed repeat startups

--- server/main.cpp
+++ server/main.cpp
@@ -12,29 +12,20 @@ vega::Log Logger;
 
 int main(int argc, char** argv) {
 
-    // attempt startup
-    int retryCount = 0;
-    bool shouldClose = false;
-    while (!shouldClose && retryCount < 5) {
-        try {
-            vega::Logger << "Starting VEGA\n";
+    try {
+        vega::Logger << "Starting VEGA\n";
 
-            // load server builder
-            vega::ServerBuilder builder;
+        // load server builder
+        vega::ServerBuilder builder;
 
-
-            shouldClose = true;
-        }
-        catch (std::exception& err) {
-            vega::Logger.SetLogLevel(vega::LogLevel::LogError);
-            std::stringstream ss;
-            ss << err.what() << "\n";
-            vega::Logger << ss.str();
-            retryCount++;
-        }
     }
+    catch (std::exception& err) {
+        vega::Logger.SetLogLevel(vega::LogLevel::LogError);
+        std::stringstream ss;
+        ss << err.what() << "\n";
+        vega::Logger << ss.str();
 
-
+    }
 
     return 0;
 }
